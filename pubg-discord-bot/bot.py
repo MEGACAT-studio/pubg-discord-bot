a651fd7c76b6c25a47b264719c3eb98d7370ff48
christopher.trotter@baerum.kommune.no <trotters.christopher@gmail.com>
2019-09-02 09:56:57 +0200

    testing the full pipeline. Github -> Travis -> Dockerhub -> rasberry pi.

--- pubg-discord-bot/bot.py
+++ pubg-discord-bot/bot.py
@@ -9,9 +9,9 @@ os.system("pwd")
 os.system("cd ..")
 os.system("ls -alt")
 # Load secrets from config/secrets.json
-#secrets = json.loads(open('/usr/src/pubg-discord-bot/config', 'r').read())
-#discordBotToken = secrets['discord-bot-token']
-#pubgApiKey = secrets['pubg-api-key']
+secrets = json.loads(open('/usr/src/pubg-discord-bot/config', 'r').read())
+discordBotToken = secrets['discord-bot-token']
+pubgApiKey = secrets['pubg-api-key']
 
 client = discord.Client()
 
